3b30f23e84901891ba9e61c2800bb11584df3640
10sr <8.slashes@gmail.com>
2023-07-28 12:16:42 +0900

    Add simple test

---
+++ rules/route53_domain_name_test.go
@@ -0,0 +1,55 @@
+package rules
+
+import (
+	"testing"
+
+	hcl "github.com/hashicorp/hcl/v2"
+	"github.com/terraform-linters/tflint-plugin-sdk/helper"
+)
+
+func Test_Route53DomainName(t *testing.T) {
+	tests := []struct {
+		Name     string
+		Content  string
+		Expected helper.Issues
+	}{
+		{
+			Name: "issue found",
+			Content: `
+resource "aws_route53_record" "invalid" {
+    type = "A"
+    name = "lnvalid_domain.example.com"
+}
+resource "aws_route53_record" "valid" {
+    type = "A"
+    name = "valid-domain.example.com"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewRoute53DomainNameRule(),
+					Message: "Invalid name for record: lnvalid_domain.example.com",
+					Range: hcl.Range{
+						Filename: "resource.tf",
+						Start:    hcl.Pos{Line: 4, Column: 12},
+						End:      hcl.Pos{Line: 4, Column: 40},
+					},
+				},
+			},
+		},
+	}
+
+	rule := NewRoute53DomainNameRule()
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			runner := helper.TestRunner(t, map[string]string{"resource.tf": test.Content})
+
+			if err := rule.Check(runner); err != nil {
+				t.Fatalf("Unexpected error occurred: %s", err)
+			}
+
+			helper.AssertIssues(t, test.Expected, runner.Issues)
+		})
+	}
+}
