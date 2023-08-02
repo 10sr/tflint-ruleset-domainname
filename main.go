14a6993a0bf9b31d4530ea963edadf9a4f88221f
10sr <8.slashes@gmail.com>
2023-08-02 22:06:35 +0900

    Prepare v0.0.3

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "domainname",
-			Version: "0.1.0",
+			Version: "0.0.3",
 			Rules: []tflint.Rule{
 				rules.NewRoute53DomainNameRule(),
 			},
